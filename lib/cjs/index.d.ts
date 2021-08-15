d51e8e3d91401558f0af2bfffe5568bc1d49a0e9
Nicolas Reyes <nicolasreyesbejarano@gmail.com>
2021-08-14 20:43:46 -0500

    new storage setup

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,5 +1,15 @@
-/// <reference types="react" />
-declare const SayHello: ({ name }: {
-    name: string;
-}) => JSX.Element;
-export default SayHello;
+import { useEffect } from 'react';
+declare type DataEffectParameters = {
+    key: string;
+    value: any;
+};
+declare type UseEffectParams = Parameters<typeof useEffect>;
+declare type EffectCallback = UseEffectParams[0];
+declare type EffectCallbackWithParameters = (data: DataEffectParameters) => void;
+declare type DependencyList = string[];
+declare type UseEffectReturn = ReturnType<typeof useEffect>;
+declare const useStorageListener: (callback: EffectCallback | EffectCallbackWithParameters, dependencies: DependencyList) => UseEffectReturn;
+export declare const setStorage: (key: string, arg: string) => void;
+export declare const removeStorage: (key: string) => void;
+export declare const getStorage: (key: string) => string | null;
+export default useStorageListener;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { useEffect } from 'react';
+type DataEffectParameters = { key: string, value: any};
+type UseEffectParams = Parameters<typeof useEffect>
+type EffectCallback = UseEffectParams[0];
+type EffectCallbackWithParameters = (data:DataEffectParameters) => void
+type DependencyList = string[];
+type UseEffectReturn = ReturnType<typeof useEffect>
 
-const SayHello = ({ name }: { name: string }): JSX.Element => (
-  <div>Hey {name}, say hello to TypeScript.</div>
-);
 
-export default SayHello;
+const useStorageListener = (callback: EffectCallback | EffectCallbackWithParameters ,
+    dependencies: DependencyList,): UseEffectReturn => {
+    useEffect(() => {
+        dependencies.forEach((key: string) => {
+            window.addEventListener(key, storageWatcher);
+        })
+        return () => {
+            dependencies.forEach((key: string) => {
+                window.removeEventListener(key, storageWatcher);
+            })
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    const storageWatcher = (e: any) => {
+        callback({key: e.type, value: getStorage(e.type)})
+    };
+}
+
+const eventDispatcher = (key: string) => {
+    return window.dispatchEvent(new Event(key, { "bubbles": true, "cancelable": true }));
+}
+
+export const setStorage = (key: string, arg: string) => {
+    if (!key) throw new Error(
+        'The storage events should not be used with no key',
+    )
+    if (!arg) throw new Error(
+        'The storage events should not be used with no arguments',
+    );
+    eventDispatcher(key);
+    localStorage.setItem(key, arg)
+}
+
+export const removeStorage = (key: string) => {
+    if (!key) throw new Error(
+        'The storage events should not be used with no key',
+    )
+    eventDispatcher(key);
+    localStorage.removeItem(key)
+}
+
+export const getStorage = (key: string) => {
+    if (!key) throw new Error(
+        'The storage events should not be used with no key',
+    )
+    return localStorage.getItem(key)
+}
+
+export default useStorageListener
