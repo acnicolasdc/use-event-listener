323fd15fbd6cda716700d8f9be16e0d505a9a4ac
Nicolas Reyes <nicolasreyesbejarano@gmail.com>
2021-08-16 11:37:25 -0500

    building proccess

---
+++ lib/cjs/index.d.ts
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+declare type DataEffectParameters = {
+    key: string;
+    value: any;
+};
+declare type UseEffectParams = Parameters<typeof useEffect>;
+declare type EffectCallback = UseEffectParams[0];
+declare type EffectCallbackWithParameters = (data: DataEffectParameters) => void;
+declare type DependencyList = string[];
+declare type UseEffectReturn = ReturnType<typeof useEffect>;
+declare const useEffectStorageListener: (callback: EffectCallback | EffectCallbackWithParameters, dependencies: DependencyList) => UseEffectReturn;
+export declare const useLocalStorage: (key: string) => {
+    state: string | null;
+    setState: import("react").Dispatch<import("react").SetStateAction<string | null>>;
+};
+/**
+ * @param key
+ * @param arg
+ */
+export declare const setStorage: (key: string, arg: unknown) => void;
+/**
+ * @param key
+ */
+export declare const removeStorage: (key: string) => void;
+/**
+ * @param key
+ * @returns
+ */
+export declare const getStorage: (key: string) => string | null;
+/**
+ * @param callEventKey
+ */
+export declare const clearStorage: (callEventKey?: string | undefined) => void;
+export default useEffectStorageListener;
